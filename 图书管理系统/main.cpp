c6a7a8ef78795275c4bf62f47686607e91789db9
FLASH <491513843@qq.com>
2020-05-22 10:00:48 +0800

    Initial Commit

---
+++ 图书管理系统/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  图书管理系统
+//
+//  Created by cdj990918 on 2020/5/22.
+//  Copyright © 2020 cdj990918. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
